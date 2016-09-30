68d4b872d7771dd294464d171eced73f689fe087
Pete Moore <pmoore@mozilla.com>
2016-09-30 21:42:21 +0200

    Added some test data used by generic worker tests

---
+++ generic-worker/check-shasums.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$(dirname "${0}")"
+
+while read sha file; do
+  if ! [ -f "${file}" ]; then
+    echo "File ${file} has not been mounted" >&2
+	exit 64
+  fi
+  shouldbe="$(shasum -a 256 "${file}")"
+  is="${sha}  ${file}"
+
+  if [ "${shouldbe}" != "${is}" ]; then
+    echo "Found:  ${sha}  ${file}" >&2
+    echo "Wanted: ${out}" >&2
+    exit 65
+  fi
+done < shasums
+
+cat shasums
+echo "All SHA256 values match expected values."
